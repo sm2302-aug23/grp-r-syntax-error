95226ff2aa23fd9f80d4eb60eeef17c02b6f5778
Hidayatullah Zakaria <19b9029@ubd.edu.bn>
2023-09-20 15:06:25 +0800

    Task 1 push

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -1,12 +1,10 @@
 # Load the necessary libraries
 library(tibble)
 
-# Create the start values from 1 to 10000
-start_values <- 1:10000
-
-# Function to generate Collatz sequences
-gen_collatz_seq <- function(n) {
+# Define the gen_collatz function
+gen_collatz <- function(n) {
   collatz_seq <- c(n)
+  
   while (n != 1) {
     if (n %% 2 == 0) {
       n <- n / 2
@@ -15,20 +13,28 @@ gen_collatz_seq <- function(n) {
     }
     collatz_seq <- c(collatz_seq, n)
   }
-  return(collatz_seq)
+  
+  return(list(seq = collatz_seq))
 }
 
-# Generate Collatz sequences for each start value
-collatz_seqs <- lapply(start_values, gen_collatz_seq)
+# Initialize an empty tibble
+collatz_df <- tibble(start = integer(0), seq = list(), length = integer(0), parity = character(0), max_val = integer(0))
 
-# Create the tibble
-collatz_df <- tibble(
-  start = start_values,
-  seq = collatz_seqs,
-  length = sapply(collatz_seqs, length),
-  parity = ifelse(sapply(collatz_seqs, function(seq) seq[length(seq)] %% 2 == 0), "Even", "Odd"),
-  max_val = sapply(collatz_seqs, max)
-)
+# Loop through integers from 1 to 10,000 and apply the gen_collatz function
+for (i in 1:10000) {
+  result <- gen_collatz(i)
+  
+  collatz_seq <- result$seq
+  collatz_df <- collatz_df %>%
+    add_row(start = i, 
+            seq = list(collatz_seq), 
+            length = length(collatz_seq), 
+            parity = ifelse(i %% 2 == 0, 
+                            "Even", 
+                            "Odd"),
+            max_val = max(collatz_seq))
+}
 
 # Print the first few rows of collatz_df
 collatz_df
+
